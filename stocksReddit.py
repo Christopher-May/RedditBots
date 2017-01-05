b83e91c773fd1346ec75697893a8e1dce6f14731
Christopher M <cjm325@mail.usask.ca>
2017-01-05 10:15:52 -0600

    A bot that checks all the comments and if a user has 0stockname in their comment this bot will respond to that comment with some information about that stock using the yahoofinance API.

---
+++ stocksReddit.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Christopher May
+"""
+import praw
+from yahoo_finance import Share
+
+
+def bot_action(name,c, verbose=True, respond=True):
+    share = Share(name.upper())
+    if share.get_price != 'none':
+        if verbose:
+            print("| Name | Price | Volume | Book Data | 200 day moving avg |\n| {n} | {p} | {v} | {b} | {t} |".format(n=share.get_name(),p=share.get_price(),v=share.get_volume(),b=share.get_book_value(),t=share.get_200day_moving_avg()))
+        if respond:
+            message = "| Name | Price | Volume | Book Data | 200 day moving avg | \n:--:|:--:|:--:|:--:|:--:|\n {n}| {p}| {v}| {b}| {t}".format(n=share.get_name(),p=share.get_price(),v=share.get_volume(),b=share.get_book_value(),t=share.get_200day_moving_avg())
+            c.reply(message)
+            
+def check_condition(c):
+    text = c.body
+    words = text.split()
+    for word in words:
+        if '$?' in word[:2]:
+                bot_action(word[2:],c)
+
+if __name__ is '__main__':
+    
+    r = praw.Reddit(user_agent = 'Stocks Moniter by ChrisM')
+    
+    r.login('jajakaha','1z=/1995')
+    
+    for c in praw.helpers.comment_stream(r,'chrisisgod'):
+        check_condition(c)
